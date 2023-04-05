3194d9d50b15afd13895392f929de440c6201b3f
hellhound <114022856+Emil-Petkov@users.noreply.github.com>
2023-04-05 15:05:49 +0300

    Create 05_hot_potato.py

---
+++ 1_stacks_and_queues/1_lab/05_hot_potato.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+kids = [name for name in input().split()]
+n_toss = int(input())
+
+queue = deque(kids)
+
+counter = 0
+
+while len(queue) > 1:
+    counter += 1
+    current_kid = queue.popleft()
+
+    if counter == n_toss:
+        print(f"Removed {current_kid}")
+        counter = 0
+    else:
+        queue.append(current_kid)
+
+print(f"Last is {queue[0]}")
