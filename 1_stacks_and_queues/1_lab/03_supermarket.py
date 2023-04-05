6e2670e8a0c1a204db31249b23b97ac48df5deba
hellhound <114022856+Emil-Petkov@users.noreply.github.com>
2023-04-05 11:59:41 +0300

    Create 03_supermarket.py

---
+++ 1_stacks_and_queues/1_lab/03_supermarket.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+people_queue = deque()
+
+while True:
+    command = input()
+
+    if command == "End":
+        print(f"{len(people_queue)} people remaining.")
+        exit()
+
+    elif command == "Paid":
+        while people_queue:
+            print(people_queue.popleft())
+
+    else:
+        people_queue.append(command)
