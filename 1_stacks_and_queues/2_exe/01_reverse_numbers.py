42a7b4d1def8b98817e0c16141a5d703bb62efdf
hellhound <114022856+Emil-Petkov@users.noreply.github.com>
2023-04-06 00:23:02 +0300

    Update 01_reverse_numbers.py

--- 1_stacks_and_queues/2_exe/01_reverse_numbers.py
+++ 1_stacks_and_queues/2_exe/01_reverse_numbers.py
@@ -1,13 +1,21 @@
-data = input().split()
+from collections import deque
 
-rev = []
-
-for num in range(len(data)):
-    l_el = data.pop()
-    rev.append(l_el)
-
+data = deque(input().split())
+rev = reversed(data)
 print(' '.join(rev))
 
+
+# data = input().split()
+#
+# rev = []
+#
+# for num in range(len(data)):
+#     l_el = data.pop()
+#     rev.append(l_el)
+#
+# print(' '.join(rev))
+#
+#
 # print(' '.join(reversed([num for num in input().split()])))
 #
 #
