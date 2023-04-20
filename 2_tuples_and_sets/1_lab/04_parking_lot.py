89f3896d81a19452d432cd25bd3e096d000a5010
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-04-20 15:41:14 +0300

    Update 04_parking_lot.py

--- 2_tuples_and_sets/1_lab/04_parking_lot.py
+++ 2_tuples_and_sets/1_lab/04_parking_lot.py
@@ -3,8 +3,9 @@ n_iteration = int(input())
 parking = set()
 
 current_car = [input().split(", ") for _ in range(n_iteration)]
-for command, reg_number in current_car: # unpacking # == current_car[0]
-                                                    # == reg_number[1]
+for command, reg_number in current_car: # unpacking 
+  # command = current_car[0]
+  # reg_number = current_car[1]
 
     if command == "IN":
         parking.add(reg_number)
