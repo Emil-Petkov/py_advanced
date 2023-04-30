6493224f7d2f67a8d53adb81ebb43099e583e85c
hellhound <114022856+Emil-Petkov@users.noreply.github.com>
2023-04-30 17:17:12 +0300

    Create matrix_comprehension.py

---
+++ 4_multidimensional_lsts/matrix_comprehension.py
@@ -0,0 +1,13 @@
+matrix = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+]
+print(matrix)  # [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]
+
+comprehension = [x + 2 for row in matrix for x in row]
+print(comprehension)  # [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+
+comprehension_matrix = [[x for x in row] for row in matrix]
+print(comprehension_matrix)  # [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]
