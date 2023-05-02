02edc3d10b8beab67a00000492957cc412798d35
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2023-05-02 19:07:37 +0300

    Create 04_sum_matrix_columns.py

---
+++ 4_multidimensional_lsts/1_lab/04_sum_matrix_columns.py
@@ -0,0 +1,14 @@
+n_rows = input().split(", ")
+
+collect_data = []
+
+for _ in range(int(n_rows[0])):
+    numbers = [int(x) for x in input().split()]
+    collect_data.append(numbers)
+
+    unpack_data = zip(*collect_data)
+
+    sum_of_matrix = [sum(unpack_data) for unpack_data in unpack_data]
+
+for el in sum_of_matrix:
+    print(el)
