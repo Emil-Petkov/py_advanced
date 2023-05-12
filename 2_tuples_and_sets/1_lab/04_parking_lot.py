0e98a01c5dde732badba22237e4141b248b86c58
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-05-12 14:55:29 +0300

    Create 04_parking_lot.py

---
+++ 2_tuples_and_sets/1_lab/04_parking_lot.py
@@ -0,0 +1,17 @@
+unique_cars = set()
+
+for _ in range(int(input())):
+    current_car = input().split(", ")
+    command = current_car[0]
+    car_number = current_car[1]
+
+    if command == "IN":
+        unique_cars.add(car_number)
+    elif command == "OUT":
+        unique_cars.remove(car_number)
+
+if unique_cars:
+    [print(car_number) for car_number in unique_cars]
+
+else:
+    print("Parking Lot is Empty")
