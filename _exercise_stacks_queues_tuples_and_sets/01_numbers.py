b88e4fb1a5ebf8695480e8df3c49d5a18a28a0c5
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2023-05-15 01:47:09 +0300

    Create 01_numbers.py

---
+++ _exercise_stacks_queues_tuples_and_sets/01_numbers.py
@@ -0,0 +1,9 @@
+first_integers = set([int(num) for num in input().split()])
+second_integers = set([int(num) for num in input().split()])
+
+for _ in range(int(input())):
+    command = input().split()
+    sets = input().split()
+    numbers = input().split()
+
+    print(data)
