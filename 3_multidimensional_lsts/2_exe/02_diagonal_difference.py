ce90e7dfb6d547027871c9f3244d6ce2a3f7d7d1
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-05-17 22:12:41 +0300

    Create 02_diagonal_difference.py

---
+++ 3_multidimensional_lsts/2_exe/02_diagonal_difference.py
@@ -0,0 +1,11 @@
+matrix = [[int(el) for el in input().split()] for _ in range(int(input()))]
+
+primary_diagonal = []
+secondary_diagonal = []
+
+for row in range(len(matrix)):
+    primary_diagonal.append(matrix[row][row])
+for row in range(len(matrix)):
+    secondary_diagonal.append(matrix[row][len(matrix) - row - 1])
+
+print(f"{abs(sum(primary_diagonal) - sum(secondary_diagonal))}")
