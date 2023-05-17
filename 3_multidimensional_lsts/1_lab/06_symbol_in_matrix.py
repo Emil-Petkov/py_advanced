c11a878f9f35f0ffe77a04c5ec6a6a1989c5bd65
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-05-17 17:24:24 +0300

    Update 06_symbol_in_matrix.py

--- 3_multidimensional_lsts/1_lab/06_symbol_in_matrix.py
+++ 3_multidimensional_lsts/1_lab/06_symbol_in_matrix.py
@@ -1,12 +1,12 @@
+def matrix_traversal(matrix, searched_symbol):
+    for row in range(len(matrix)):
+        for col in range(len(matrix)):
+            if matrix[row][col] == searched_symbol:
+                return f"({row}, {col})"
+
+    return f"{searched_symbol} does not occur in the matrix"
+
+
 matrix = [[el for el in input()] for _ in range(int(input()))]
 
-searched_element = input()
-
-for row in range(len(matrix)):
-    for col in range(len(matrix)):
-        if matrix[row][col] == searched_element:
-            print(f"({row}, {col})")
-            exit()
-else:
-    print(f"{searched_element} does not occur in the matrix")
-    exit()
+print(matrix_traversal(matrix, searched_symbol=input()))
