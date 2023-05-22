3b2f6490a6c73212e01354d6a2a3f04f1792c59f
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-05-23 02:13:32 +0300

    Update 04_water_dispenser.py

--- 1_stacks_and_queues/1_lab/04_water_dispenser.py
+++ 1_stacks_and_queues/1_lab/04_water_dispenser.py
@@ -20,3 +20,34 @@ while queue:
         print(f"{queue.popleft()} must wait")
 
 print(f"{dispenser_quantity} liters left")
+
+#############################################################################
+
+# from collections import deque
+
+# quantity_water = int(input())
+
+# queue = deque()
+
+# command = input()
+
+# while not command == "Start":
+#     queue.append(command)
+
+#     command = input()
+
+# get_water = input().split()
+# while not get_water[0] == "End":
+#     if get_water[0] == "refill":
+#         quantity_water += int(get_water[1])
+#     else:
+
+#         if quantity_water >= int(get_water[0]):
+#             quantity_water -= int(get_water[0])
+#             print(f"{queue.popleft()} got water")
+#         else:
+#             print(f"{queue.popleft()} must wait")
+
+#     get_water = input().split()
+
+# print(f"{quantity_water} liters left")
