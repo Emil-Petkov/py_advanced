dad2711b57a445106f93d921386dacc08d285311
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-05-23 16:20:01 +0300

    Update 01_reverse_numbers.py

--- 1_stacks_and_queues/2_exe/01_reverse_numbers.py
+++ 1_stacks_and_queues/2_exe/01_reverse_numbers.py
@@ -10,6 +10,20 @@
 
 ######################################################
 
+# from collections import deque
+
+# numbers = input().split()
+
+# reverse_numbers = deque()
+
+# for num in numbers:
+#     reverse_numbers.append(num)
+
+# while reverse_numbers:
+#     print(reverse_numbers.pop(), end=" ")
+
+#####################################################
+
 from collections import deque
 
 numbers = deque(input().split())
