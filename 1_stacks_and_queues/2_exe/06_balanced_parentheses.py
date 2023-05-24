a9fc7526ac9e7b805bf55b79bc7fccd1d19fb138
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-05-24 15:49:12 +0300

    Update 06_balanced_parentheses.py

--- 1_stacks_and_queues/2_exe/06_balanced_parentheses.py
+++ 1_stacks_and_queues/2_exe/06_balanced_parentheses.py
@@ -1,20 +1,43 @@
-from collections import deque
+def check_brackets(check):
+    bracket_map = {"(": ")", "[": "]", "{": "}"}
+    open_brackets = bracket_map.keys()
 
-parenthesis = (deque(input()))
-open_parenthesis = deque()
+    stack = []
 
-while parenthesis:
-    current_parenthesis = parenthesis.popleft()
+    for br in check:
+        if br in open_brackets:
+            stack.append(br)
+        elif stack and br == bracket_map[stack[-1]]:
+            stack.pop()
+        else:
+            return "NO"
 
-    if current_parenthesis in "{[(":
-        open_parenthesis.append(current_parenthesis)
+    return "YES"
 
-    elif not open_parenthesis:
-        print("NO")
-        break
-    else:
-        if f"{open_parenthesis.pop() + current_parenthesis}" not in '(){}[]':
-            print("NO")
-            break
-else:
-    print("YES")
+
+print(check_brackets(input()))
+
+
+##################################################################
+
+
+# from collections import deque
+
+# parenthesis = (deque(input()))
+# open_parenthesis = deque()
+
+# while parenthesis:
+#     current_parenthesis = parenthesis.popleft()
+
+#     if current_parenthesis in "{[(":
+#         open_parenthesis.append(current_parenthesis)
+
+#     elif not open_parenthesis:
+#         print("NO")
+#         break
+#     else:
+#         if f"{open_parenthesis.pop() + current_parenthesis}" not in '(){}[]':
+#             print("NO")
+#             break
+# else:
+#     print("YES")
