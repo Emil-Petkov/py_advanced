e071f7d9c20e52855a629d1fbf45bbad523c143f
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-05-24 20:04:09 +0300

    Update 03_record_unique_names.py

--- 2_tuples_and_sets/1_lab/03_record_unique_names.py
+++ 2_tuples_and_sets/1_lab/03_record_unique_names.py
@@ -5,3 +5,20 @@ for _ in range(int(input())):
     unique_names.add(current_name)
 
 print("\n".join(unique_names))
+
+
+######################################################
+
+# def unique_names(n):
+#     collection_unique_names = set()
+
+#     for _ in range(n):
+#         current_name = input()
+#         if current_name not in collection_unique_names:
+#             collection_unique_names.add(current_name)
+
+#     for name in collection_unique_names:
+#         print(name)
+
+
+# unique_names(int(input()))
