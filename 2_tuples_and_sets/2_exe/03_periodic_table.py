814956232015ebfbef1c3d26a0f623ca378fffbd
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-05-25 12:42:46 +0300

    Update 03_periodic_table.py

--- 2_tuples_and_sets/2_exe/03_periodic_table.py
+++ 2_tuples_and_sets/2_exe/03_periodic_table.py
@@ -1,10 +1,17 @@
-n = int(input())
-unique_elements = set()
+elements = {el for _ in range(int(input())) for el in input().split()}
 
-for i in range(n):
-    compounds = input().split()
-    for element in compounds:
-        unique_elements.add(element)
-
-for element in unique_elements:
+for element in elements:
     print(element)
+
+#####################################################################
+
+# n = int(input())
+# unique_elements = set()
+
+# for i in range(n):
+#     compounds = input().split()
+#     for element in compounds:
+#         unique_elements.add(element)
+
+# for element in unique_elements:
+#     print(element)
