21d1cc43537f4e02efab136bc3d4ba72f0134acc
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-05-27 17:37:44 +0300

    Delete 01_numbers.py

--- _exercise_stacks_queues_tuples_and_sets/01_numbers.py
+++
@@ -1,9 +0,0 @@
-first_integers = set([int(num) for num in input().split()])
-second_integers = set([int(num) for num in input().split()])
-
-for _ in range(int(input())):
-    command = input().split()
-    sets = input().split()
-    numbers = input().split()
-
-    print(data)
