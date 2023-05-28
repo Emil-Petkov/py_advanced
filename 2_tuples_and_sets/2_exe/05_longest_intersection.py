2da3bbc9106c87c7ee43ef57945e389e8b0ac106
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-05-28 17:51:20 +0300

    Update 05_longest_intersection.py

--- 2_tuples_and_sets/2_exe/05_longest_intersection.py
+++ 2_tuples_and_sets/2_exe/05_longest_intersection.py
@@ -16,17 +16,11 @@ longest_intersection = []
 for _ in range(n_lines):
     current_range = input().split("-")
 
-    first_set = set()
-    second_set = set()
-
     first_start, first_end = [int(x) for x in current_range[0].split(",")]
     second_start, second_end = [int(x) for x in current_range[1].split(",")]
 
-    for x in range(first_start, first_end + 1):
-        first_set.add(x)
-
-    for y in range(second_start, second_end + 1):
-        second_set.add(y)
+    first_set = set(range(first_start, first_end + 1))
+    second_set = set(range(second_start, second_end + 1))
 
     intersection = first_set.intersection(second_set)
 
@@ -34,4 +28,3 @@ for _ in range(n_lines):
         longest_intersection = intersection
 
 print(f"Longest intersection is {list(longest_intersection)} with length {len(longest_intersection)}")
-
