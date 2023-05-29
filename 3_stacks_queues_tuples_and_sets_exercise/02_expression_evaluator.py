4817a2e55f13b5cb9865e0269715dcc3c83f92b5
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-05-29 16:25:04 +0300

    Create 02_expression_evaluator.py

---
+++ 3_stacks_queues_tuples_and_sets_exercise/02_expression_evaluator.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+expression = input().split()
+copy_expression = expression.copy()
+
+nums = deque()
+
+map_operation = {
+    "+": lambda a, b: a + b,
+    "-": lambda a, b: a - b,
+    "*": lambda a, b: a * b,
+    "/": lambda a, b: a // b,
+}
+
+for el in expression:
+    if el in "+-*/":
+
+        while len(nums) > 1:
+            left = nums.popleft()
+            right = nums.popleft()
+            result = map_operation[el](left, right)
+            nums.appendleft(result)
+
+
+
+    else:
+        nums.append(int(el))
+
+print(nums.pop())
