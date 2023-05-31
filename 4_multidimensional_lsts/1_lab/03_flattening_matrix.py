f478d320fe6014f1ea8c85ca66c243bac6c37f86
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-05-31 19:30:08 +0300

    Update 03_flattening_matrix.py

--- 4_multidimensional_lsts/1_lab/03_flattening_matrix.py
+++ 4_multidimensional_lsts/1_lab/03_flattening_matrix.py
@@ -1,3 +1,16 @@
 data = [[int(numbers) for numbers in input().split(", ")] for _ in range(int(input()))]
 
 print([num for row in data for num in row])
+
+#############################################################
+
+# row =  int(input())
+
+# result = []
+
+# for _ in range(row):
+#     current_row = input().split(", ")
+#     for el in current_row:
+#         result.append(int(el))
+
+# print(result)
