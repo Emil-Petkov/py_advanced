2c1d745fc9a17469136d8dc31dc93f463c6f8123
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-06-08 11:13:47 +0300

    Update 01_diagonals.py

--- 4_multidimensional_lsts/2_exe/01_diagonals.py
+++ 4_multidimensional_lsts/2_exe/01_diagonals.py
@@ -1,12 +1,13 @@
-matrix = [[int(el) for el in input().split(", ")] for _ in range(int(input()))]
+matrix = [[int(x) for x in input().split(", ")] for _ in range(int(input()))]
 
 primary_diagonal = []
 secondary_diagonal = []
 
 for row in range(len(matrix)):
     primary_diagonal.append(matrix[row][row])
+
 for row in range(len(matrix)):
     secondary_diagonal.append(matrix[row][len(matrix) - row - 1])
 
-print(f"Primary diagonal: {', '.join((str(el) for el in primary_diagonal))}. Sum: {sum(primary_diagonal)}")
-print(f"Secondary diagonal: {', '.join((str(el) for el in secondary_diagonal))}. Sum: {sum(secondary_diagonal)}")
+print(f"Primary diagonal: {', '.join(str(x) for x in primary_diagonal)}. Sum: {sum(primary_diagonal)}")
+print(f"Secondary diagonal: {', '.join(str(x) for x in secondary_diagonal)}. Sum: {sum(secondary_diagonal)}")
